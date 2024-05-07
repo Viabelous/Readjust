9d3357cee10406c06838806ba46f99c18019f26b
Natalieefd <nataliefuad2003@gmail.com>
2024-05-07 22:25:15 +0800

    feat: scene Main Menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("DeveloperZone");
+    }
+
+    // Update is called once per frame
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
