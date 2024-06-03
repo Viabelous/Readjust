fabc17ecd1c530a7ad26a81c422dd9830d59c6bf
Viabel / Yafi Arya <ibnu.yafi.aw@gmail.com>
2024-06-03 23:12:27 +0800

    fix: skill slots position bug

--- Assets/Scripts/UI/RewardPanel.cs
+++ Assets/Scripts/UI/RewardPanel.cs
@@ -1,4 +1,3 @@
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
--- Assets/Scripts/UI/SkillUpgradeWindowsController.cs
+++ Assets/Scripts/UI/SkillUpgradeWindowsController.cs
@@ -1,4 +1,3 @@
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
 using UnityEngine.UI;
 
